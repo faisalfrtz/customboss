39e99968c3158105eae161ab7fd3e5e4438b9c48
faisalfrtz <anandafaisalf20>
2019-06-26 23:17:03 +0700

    cek

---
+++ application/controllers/LandingPage.php
@@ -0,0 +1,54 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class LandingPage extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$data = [
+			'pageContent' => 'home',
+		];
+		$this->load->view('customboss/layout/master',$data);
+	}
+
+	public function about()
+	{
+		$data = [
+			'pageContent' => 'about',
+		];
+		$this->load->view('customboss/layout/master',$data);
+	}
+
+	public function gallery()
+	{
+		$data = [
+			'pageContent' => 'gallery',
+			'bodyClass' => 'gallery-page',
+		];
+		$this->load->view('customboss/layout/master',$data);
+	}
+
+	public function contact()
+	{
+		$data = [
+			'pageContent' => 'contact',
+			'bodyClass' => 'contact-page',
+		];
+		$this->load->view('customboss/layout/master',$data);
+	}
+}
---
+++ readme.rst
@@ -0,0 +1,1 @@
+Custom Boss Group @2019
